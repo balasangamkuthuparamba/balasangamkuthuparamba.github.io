076bd3764139d428c4de86a4586f19ea98a41d81
akshaypalukachipara <akshaypalukachipara@gmail.com>
2020-09-16 14:01:00 +0530

    nav style edited

--- script/script.js
+++ script/script.js
@@ -1,17 +1,4 @@
 
-$(document).on("scroll", function(){
-    if
-  ($(document).scrollTop() > 86){
-      $("#banner").addClass("shrink");
-    }
-    else
-    {
-        $("#banner").removeClass("shrink");
-    }
-});
-
-
-
   $(document).ready(function() {
   
     setTimeout(function() {
